df9734d566d974ba4aa236ee8b97861045200083
loto4uck <101703715+loto4uck@users.noreply.github.com>
2022-03-17 11:05:11 +0200

    Add files via upload

---
+++ zavd7.java
@@ -0,0 +1,19 @@
+package whille;
+
+import java.util.Scanner;
+
+public class zavd7 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int p = sc.nextInt();
+		int y = sc.nextInt();
+		int d = 0;
+		while (x <= y) {
+			x = (x * (100 + p) / 100);
+			d += 1;
+		}System.out.println(d);
+			
+		}
+
+}
