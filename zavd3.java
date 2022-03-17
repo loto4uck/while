01ba97abe2f799335cfb9c920ab4395a00e1dd75
loto4uck <101703715+loto4uck@users.noreply.github.com>
2022-03-17 11:00:49 +0200

    Add files via upload

---
+++ zavd3.java
@@ -0,0 +1,17 @@
+package whille;
+
+import java.util.Scanner;
+
+public class zavd3 {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = 1;
+		while (b < a ) {
+			System.out.println(b);
+			b *= 2;
+		}
+		
+	}
+
+}
